db1e48a7eb716d50e6e3325cdabfff24d16f4fe5
bru41 <valkenburgnick@gmail.com>
2025-01-26 19:26:51 +0100

    Add files via upload

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+
+  description = "The first falke";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  };
+
+  outputs = { self, nixpkgs , ... }:
+    let
+      lib = nixpkgs.lib;
+      pkgs = nixpkgs.pkgs;
+    in{
+      nixosConfigurations = {
+        nixos-test = lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [ ./configuration.nix ];
+        };
+      };
+#      hardware.pulseaudio.enable = true;
+#      services.xserver.enable = true;
+#      services.displayManager.sddm.enable = true;
+#      services.desktopManager.plasma6.enable = true;
+#      enviroment.plasma6.excludePackages = with pkgs.kdePackages;[ plasma-browser-intergration oxygen];
+    };
+
+}
